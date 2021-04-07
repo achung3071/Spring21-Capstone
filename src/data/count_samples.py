4f9b4f0963f4bcf6a2cc9d61a54ce41dcc9c3c6d
Andrew Chung <drew.c3071@gmail.com>
2021-04-07 16:30:49 +0900

    Change project directory structure (cookiecutter)

---
+++ src/data/count_samples.py
@@ -0,0 +1,20 @@
+# Script for counting the number of images in each split.
+# Output shown in `data/raw/sample_counts.json` from the root directory.
+
+import json
+import os
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent.parent.parent
+IMAGE_SPLITS_DIR = f'{ROOT_DIR}/data/raw/image_splits'
+sample_counts = {}
+
+for file in os.listdir(IMAGE_SPLITS_DIR):
+    with open(f'{IMAGE_SPLITS_DIR}/{file}', 'r') as read_file:
+        SEGMENT = '.'.join(file.split('.')[1:3])
+        data = json.load(read_file)
+        sample_counts[SEGMENT] = len(data)
+
+sample_counts = dict(sorted(sample_counts.items()))
+with open(f'{ROOT_DIR}/data/raw/sample_counts.json', 'w') as out_file:
+    json.dump(sample_counts, out_file)
